aa254779dddc6d594122476abd20a6552b0f7265
Owen Busler <owen1050@gmail.com>
2024-01-28 02:33:31 -0500

    needs cleanup, but simulation looks good with holonomic controller

--- FRC8513_2024/src/main/java/frc/robot/AutoController.java
+++ FRC8513_2024/src/main/java/frc/robot/AutoController.java
@@ -1,14 +1,22 @@
 package frc.robot;
 
+import javax.swing.text.StyleContext.SmallAttributeSet;
+
 import com.pathplanner.lib.path.PathPlannerPath;
 import com.pathplanner.lib.path.PathPlannerTrajectory;
 import com.pathplanner.lib.path.PathPlannerTrajectory.State;
 
+import edu.wpi.first.math.controller.HolonomicDriveController;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class AutoController {
 
@@ -18,6 +26,7 @@ public class AutoController {
     PIDController headingPidController;
     PathPlannerTrajectory autoTraj;
     int pathNum = 0;
+    HolonomicDriveController holonomicDriveController;
 
 
     public AutoController(Robot thisRobot_){
@@ -27,7 +36,12 @@ public class AutoController {
     public void autoInit(){
         headingPidController  = new PIDController(Settings.hc_P, Settings.hc_I, Settings.hc_D);
 
-        path = PathPlannerPath.fromPathFile("SubWooferToFarNote");
+        holonomicDriveController = new HolonomicDriveController(
+        new PIDController(20, 0.5, 0), new PIDController(20, 0.1, 0),
+        new ProfiledPIDController(5, 0, 0,
+            new TrapezoidProfile.Constraints(thisRobot.drivebase.swerveDrive.getMaximumAngularVelocity(), 6.28)));
+
+        path = PathPlannerPath.fromPathFile("SillyPath");
         Pose2d initPose = path.getPreviewStartingHolonomicPose();
         thisRobot.drivebase.swerveDrive.resetOdometry(initPose);
         trajStartTime = Timer.getFPGATimestamp();
@@ -43,11 +57,24 @@ public class AutoController {
             State goalState = autoTraj.sample(trajElapsedTime);
             Rotation2d currentHeading = thisRobot.drivebase.swerveDrive.field.getRobotPose().getRotation();
             Rotation2d goalHeading = goalState.targetHolonomicRotation;
+
+            SmartDashboard.putNumber("goalX", goalState.positionMeters.getX());
+            
+            SmartDashboard.putNumber("goalY", goalState.positionMeters.getY());
+            
+            Trajectory.State trajState = new Trajectory.State(goalState.timeSeconds,
+                goalState.velocityMps,
+                goalState.accelerationMpsSq,
+                new Pose2d(goalState.positionMeters,goalHeading),
+            goalState.curvatureRadPerMeter);
+
+            ChassisSpeeds adjustedSpeeds = holonomicDriveController.calculate(
+            thisRobot.drivebase.swerveDrive.getPose(), trajState, goalHeading);
             
             double rotCorrectionOutput = headingPidController.calculate(currentHeading.getDegrees(), goalHeading.getDegrees());
-            thisRobot.drivebase.swerveDrive.drive( new Translation2d(goalState.velocityMps, goalState.heading), rotCorrectionOutput,true,  false);
-            
-            if(trajElapsedTime > autoTraj.getTotalTimeSeconds() && pathNum < 1){
+            //thisRobot.drivebase.swerveDrive.drive( new Translation2d(goalState.velocityMps, goalState.heading), rotCorrectionOutput,true,  false);
+            thisRobot.drivebase.swerveDrive.drive(adjustedSpeeds);
+            if(trajElapsedTime > autoTraj.getTotalTimeSeconds() && pathNum < 0){
                 path = PathPlannerPath.fromPathFile("FarNoteToFirstNoteAndBack");
                 autoTraj =  path.getTrajectory(thisRobot.drivebase.swerveDrive.getFieldVelocity(), thisRobot.drivebase.swerveDrive.getOdometryHeading());
                 pathNum++;
