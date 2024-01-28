200d3260b950fa3b4cc228414cc79242be88b04e
Owen Busler <owen1050@gmail.com>
2024-01-28 02:07:58 -0500

    Cant use ramsite with swerve (who knew)

--- FRC8513_2024/src/main/java/frc/robot/AutoController.java
+++ FRC8513_2024/src/main/java/frc/robot/AutoController.java
@@ -5,14 +5,10 @@ import com.pathplanner.lib.path.PathPlannerTrajectory;
 import com.pathplanner.lib.path.PathPlannerTrajectory.State;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.RamseteController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class AutoController {
 
@@ -20,8 +16,8 @@ public class AutoController {
     double trajStartTime = -1;
     PathPlannerPath path;
     PIDController headingPidController;
-    RamseteController ramseteController = new RamseteController(2.1, 0.8);
     PathPlannerTrajectory autoTraj;
+    int pathNum = 0;
 
 
     public AutoController(Robot thisRobot_){
@@ -29,13 +25,14 @@ public class AutoController {
     }
 
     public void autoInit(){
-        path = PathPlannerPath.fromPathFile("TestPath");
+        headingPidController  = new PIDController(Settings.hc_P, Settings.hc_I, Settings.hc_D);
+
+        path = PathPlannerPath.fromPathFile("SubWooferToFarNote");
         Pose2d initPose = path.getPreviewStartingHolonomicPose();
         thisRobot.drivebase.swerveDrive.resetOdometry(initPose);
-        headingPidController  = new PIDController(Settings.hc_P, Settings.hc_I, Settings.hc_D);
         trajStartTime = Timer.getFPGATimestamp();
         autoTraj =  path.getTrajectory(thisRobot.drivebase.swerveDrive.getFieldVelocity(), thisRobot.drivebase.swerveDrive.getOdometryHeading());
-        
+        pathNum = 0;
     }
 
     public void autoPeriodic(){
@@ -46,25 +43,18 @@ public class AutoController {
             State goalState = autoTraj.sample(trajElapsedTime);
             Rotation2d currentHeading = thisRobot.drivebase.swerveDrive.field.getRobotPose().getRotation();
             Rotation2d goalHeading = goalState.targetHolonomicRotation;
-
-            Trajectory.State trajState = new Trajectory.State(goalState.timeSeconds,
-            goalState.velocityMps,
-            goalState.accelerationMpsSq,
-            goalState.getTargetHolonomicPose(),
-            goalState.curvatureRadPerMeter);
-
-            ChassisSpeeds adjustedChassiSpeeds = ramseteController.calculate(
-                thisRobot.drivebase.swerveDrive.field.getRobotPose(),
-                trajState);
             
             double rotCorrectionOutput = headingPidController.calculate(currentHeading.getDegrees(), goalHeading.getDegrees());
-            SmartDashboard.putNumber("rotCoroutput", rotCorrectionOutput);
-            SmartDashboard.putNumber("rotError", goalHeading.getDegrees() - currentHeading.getDegrees());
-            SmartDashboard.putNumber("goalHeading", goalHeading.getDegrees());
-            SmartDashboard.putNumber("goalV", goalState.velocityMps);
-            // this is for no ramsete thisRobot.drivebase.swerveDrive.drive( new Translation2d(goalState.velocityMps, goalState.heading), rotCorrectionOutput,true,  false);
-             thisRobot.drivebase.swerveDrive.drive(adjustedChassiSpeeds);
-        
+            thisRobot.drivebase.swerveDrive.drive( new Translation2d(goalState.velocityMps, goalState.heading), rotCorrectionOutput,true,  false);
+            
+            if(trajElapsedTime > autoTraj.getTotalTimeSeconds() && pathNum < 1){
+                path = PathPlannerPath.fromPathFile("FarNoteToFirstNoteAndBack");
+                autoTraj =  path.getTrajectory(thisRobot.drivebase.swerveDrive.getFieldVelocity(), thisRobot.drivebase.swerveDrive.getOdometryHeading());
+                pathNum++;
+                trajStartTime = Timer.getFPGATimestamp();
+            }
+
+
         } else {
             //drive manually
             thisRobot.drivebase.swerveDrive.drive(
