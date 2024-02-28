2eb09001f29fa366baef46cda142786089d309cb
Sisters1st <89617423+Sisters1st@users.noreply.github.com>
2024-02-27 19:03:53 -0500

    new button pannel

--- FRC8513_2024/src/main/java/frc/robot/Settings.java
+++ FRC8513_2024/src/main/java/frc/robot/Settings.java
@@ -50,16 +50,16 @@ public class Settings {
     // Button pannel settings
     public static int buttonPannelPort = 1;
 
-    public static int drivingStateReturnButton = 1;
-    public static int intakeButton = 2;
-    public static int shootInSpeakerButton = 3;
-    public static int ampPrepButton = 4;
-    public static int climberPrepButton = 5;
-    public static int intakeOutButton = 6;
-    public static int climbUpButton = 7;
-    public static int climbDownButton = 8;
-    public static int runFeederInButton = 9;
-    public static int runFeederOutButton = 10;
+    public static int drivingStateReturnButton = 6;
+    public static int intakeButton = 7;
+    public static int shootInSpeakerButton = 8;
+    public static int ampPrepButton = 9;
+    public static int climberPrepButton = 10;
+    public static int intakeOutButton = 3;
+    public static int climbUpButton = 1;
+    public static int climbDownButton = 2;
+    public static int runFeederInButton = 4;
+    public static int runFeederOutButton = 5;
 
     // manual overide joystick settings
     public static int manualControlPort = 2;
